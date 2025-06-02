0b297f2f55ccc65cb007a7927d73f4a7147fa8f3
David Camacho <camacho.david.g@gmail.com>
2025-06-01 21:46:13 -0700

    updated api project

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
-fetch('https://api.github.com/users/dcamacho2002/repos')
-    .then(function (response) {
+const display1 = document.getElementById("data1");
+
+fetch('https://www.swapi.tech/api/planets')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
         return response.json();
     })
-    .then(function (repositories) {
-        console.log(repositories);
-
-        const projectSection = document.getElementById("Projects");
-        const projectList = projectSection.querySelector("ul");
-
-        for (let i = 0; i < repositories.length; i++) {
-            const project = document.createElement("li");
-            const link = document.createElement("a");
-            link.href = repositories[i].html_url;
-            link.innerText = repositories[i].name;
-            link.target = "_blank";
-            project.appendChild(link);
-            projectList.appendChild(project);
-        }
+    .then(data => {
+        display1.innerHTML = "<h2>Notable Planets</h2><ul>";
+        data.results.forEach((planet) => {
+            fetch(planet.url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Request failed');
+                    }
+                    return response.json();
+                })
+                .then((planetData) => {
+                    display1.innerHTML += `<li>
+                    ${planetData.result.properties.name}
+                    <ul>
+                    <li>Climate: ${planetData.result.properties.climate}</li>
+                    <li>Population: ${planetData.result.properties.population}</li>
+                    <li>Terrain: ${planetData.result.properties.terrain}</li>
+                    </ul>
+                    </li>`;
+                })
+                .catch(error => {
+                    console.error('An error occurred:', error);
+                })
+        })
+        display1.innerHTML += "</ul>";
+    })
+    .catch(error => {
+        console.error('An error occurred:', error);
     })
 
-    .catch(function (error) {
+const display2 = document.getElementById("data2");
+
+fetch('https://www.swapi.tech/api/people')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        return response.json();
+    })
+    .then(data => {
+        display2.innerHTML = "<h2>Notable Characters</h2><ul>";
+        data.results.forEach((people) => {
+            fetch(people.url)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Request failed');
+                    }
+                    return response.json();
+                })
+                .then((peopleData) => {
+                    display2.innerHTML += `<li>
+                    ${peopleData.result.properties.name}
+                    <ul>
+                    <li>Birth Year: ${peopleData.result.properties.birth_year}</li>
+                    <li>Gender: ${peopleData.result.properties.gender}</li>
+                    <li>Height: ${peopleData.result.properties.height}cm</li>
+                    </ul>
+                    </li>`;
+                })
+                .catch(error => {
+                    console.error('An error occurred:', error);
+                })
+        })
+        display2.innerHTML += "</ul>";
+    })
+    .catch(error => {
         console.error('An error occurred:', error);
-        console.log('Project section empty.');
-    }) 
+    })
