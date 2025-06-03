70e916668627e085034c0d012dc161167ede539c
David Camacho <camacho.david.g@gmail.com>
2025-06-03 15:39:17 -0700

    final project changes

--- index.js
+++ index.js
@@ -1,77 +1,85 @@
-const display1 = document.getElementById("data1");
+const display1 = document.getElementById("data");
+const planetButton = document.getElementById("planetButton");
+const characterButton = document.getElementById("characterButton");
+const display2 = document.getElementById("data");
 
-fetch('https://www.swapi.tech/api/planets')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Request failed');
-        }
-        return response.json();
-    })
-    .then(data => {
-        display1.innerHTML = "<h2>Notable Planets</h2><ul>";
-        data.results.forEach((planet) => {
-            fetch(planet.url)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Request failed');
-                    }
-                    return response.json();
-                })
-                .then((planetData) => {
-                    display1.innerHTML += `<li>
-                    ${planetData.result.properties.name}
-                    <ul>
-                    <li>Climate: ${planetData.result.properties.climate}</li>
-                    <li>Population: ${planetData.result.properties.population}</li>
-                    <li>Terrain: ${planetData.result.properties.terrain}</li>
-                    </ul>
-                    </li>`;
-                })
-                .catch(error => {
-                    console.error('An error occurred:', error);
-                })
+// Notable planets button
+planetButton.addEventListener('click', function() {
+    fetch('https://www.swapi.tech/api/planets')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Request failed');
+            }
+            return response.json();
         })
-        display1.innerHTML += "</ul>";
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    })
-
-const display2 = document.getElementById("data2");
-
-fetch('https://www.swapi.tech/api/people')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Request failed');
-        }
-        return response.json();
-    })
-    .then(data => {
-        display2.innerHTML = "<h2>Notable Characters</h2><ul>";
-        data.results.forEach((people) => {
-            fetch(people.url)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Request failed');
-                    }
-                    return response.json();
-                })
-                .then((peopleData) => {
-                    display2.innerHTML += `<li>
-                    ${peopleData.result.properties.name}
-                    <ul>
-                    <li>Birth Year: ${peopleData.result.properties.birth_year}</li>
-                    <li>Gender: ${peopleData.result.properties.gender}</li>
-                    <li>Height: ${peopleData.result.properties.height}cm</li>
-                    </ul>
-                    </li>`;
-                })
-                .catch(error => {
-                    console.error('An error occurred:', error);
-                })
+        .then(data => {
+            display1.innerHTML = "<h2>Notable Planets</h2><ul>";
+            data.results.forEach((planet) => {
+                fetch(planet.url)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Request failed');
+                        }
+                        return response.json();
+                    })
+                    .then((planetData) => {
+                        display1.innerHTML += `<li>
+                        ${planetData.result.properties.name}
+                        <ul>
+                        <li>Climate: ${planetData.result.properties.climate}</li>
+                        <li>Population: ${planetData.result.properties.population}</li>
+                        <li>Terrain: ${planetData.result.properties.terrain}</li>
+                        </ul>
+                        </li>`;
+                    })
+                    .catch(error => {
+                        console.error('An error occurred:', error);
+                    })
+            })
+            display1.innerHTML += "</ul>";
         })
-        display2.innerHTML += "</ul>";
-    })
-    .catch(error => {
-        console.error('An error occurred:', error);
-    })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        })
+});
+
+
+// Notable characters button
+characterButton.addEventListener('click', function() {
+    fetch('https://www.swapi.tech/api/people')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Request failed');
+            }
+            return response.json();
+        })
+        .then(data => {
+            display2.innerHTML = "<h2>Notable Characters</h2><ul>";
+            data.results.forEach((people) => {
+                fetch(people.url)
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Request failed');
+                        }
+                        return response.json();
+                    })
+                    .then((peopleData) => {
+                        display2.innerHTML += `<li>
+                        ${peopleData.result.properties.name}
+                        <ul>
+                        <li>Birth Year: ${peopleData.result.properties.birth_year}</li>
+                        <li>Gender: ${peopleData.result.properties.gender}</li>
+                        <li>Height: ${peopleData.result.properties.height}cm</li>
+                        </ul>
+                        </li>`;
+                    })
+                    .catch(error => {
+                        console.error('An error occurred:', error);
+                    })
+            })
+            display2.innerHTML += "</ul>";
+        })
+        .catch(error => {
+            console.error('An error occurred:', error);
+        })
+});
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # david-camacho-kepler
 API project for Intro to Programming course with Code the Dream (https://www.swapi.tech/)
 David Camacho
+
+How to run:
+
+1. Download repository
+
+2. Open index.html in web browser
+
+3. Use navigation section to take you to a list of characters or planets featured in Star Wars
+	- Each navigation button will load 10 characters/planets at random
